6a73b2b77faf960765640c843ee96bb5beeee83d
phegorez <metha.nompd@gmail.com>
2024-04-22 20:00:19 +0700

    save

--- components/FeaturesItem.tsx
+++ components/FeaturesItem.tsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import Image from 'next/image'
 type FeaturesItemProps = {
-    key: number
     title: string,
     icon: string,
     variant: string
     description: string
 }
 
-function FeaturesItem({ key, title, icon, variant, description }: FeaturesItemProps) {
+function FeaturesItem({title, icon, variant, description }: FeaturesItemProps) {
     return (
         <li className='flex w-full flex-1 flex-col items-start'>
             <div className='rounded-full p-4 lg:p-7 bg-green-50'>
@@ -19,7 +18,7 @@ function FeaturesItem({ key, title, icon, variant, description }: FeaturesItemPr
                     width={28}
                 />
             </div>
-            <h2 className='bold-20 lg:bold-32 mt-5 capitalize'>
+            <h2 className={`bold-20 lg:bold-32 mt-5 capitalize ${variant}`}>
                 {title}
             </h2>
             <p className='regular-16 mt-5 bg-white/80 text-gray-30 lg:mt-[30px] lg:bg-none'>
