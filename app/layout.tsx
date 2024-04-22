3cfd5786721d9fbef26f78f776e6cdcde42c2400
phegorez <metha.nompd@gmail.com>
2024-04-22 19:09:27 +0700

    save

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
-  title: "Travel",
+  title: "Sanctuary Travel",
   description: "Travel UI/UX App for Camping",
 };
 
