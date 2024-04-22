a8d3f4c4e5daec3502e7251add6e3bf4d3957539
phegorez <metha.nompd@gmail.com>
2024-04-22 19:03:23 +0700

    save

---
+++ components/FooterLink.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+import React from 'react'
+type FooterLinkProps = {
+    title: string,
+    children: React.ReactNode
+}
+
+function FooterLink({ title, children }: FooterLinkProps) {
+    return (
+        <div className='flex flex-col gap-5'>
+            <h4 className='bold-18 whitespace-nowrap'>{title}</h4>
+            {children}
+        </div>
+    )
+}
+
+export default FooterLink
